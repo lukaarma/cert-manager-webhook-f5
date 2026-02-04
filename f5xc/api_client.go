3df4e3a0f69b50838b1062afe4ba5ab6c664efc7
lukaarma <lukaarma@gmail.com>
2026-02-04 01:29:51 +0100

    Implement F5Xc API client methods required to interact with TXT records

--- f5xc/api_client.go
+++ f5xc/api_client.go
@@ -1,24 +1,29 @@
 package f5xc
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
+
+	"k8s.io/klog/v2"
 )
 
 const (
-	BaseURL = "https://%s.console.ves.volterra.io/api"
+	baseUrl = "https://%s.console.ves.volterra.io/api"
 )
 
-func NewClient(tenantName string, apiKey string) *F5XCClient {
-	return &F5XCClient{
-		BaseURL: fmt.Sprintf(BaseURL, tenantName),
+func NewClient(tenantName string, apiKey string) *f5xcClient {
+	return &f5xcClient{
+		BaseURL: fmt.Sprintf(baseUrl, tenantName),
 		ApiKey:  apiKey,
 		Client:  &http.Client{},
 	}
+
 }
 
-func (c *F5XCClient) send(req *http.Request, resData interface{}) error {
+func (c *f5xcClient) send(req *http.Request, resData interface{}) error {
 	req.Header.Set("Content-Type", "application/json; charset=utf-8")
 	req.Header.Set("Accept", "application/json; charset=utf-8")
 	req.Header.Set("Authorization", fmt.Sprintf("APIToken %s", c.ApiKey))
@@ -30,13 +35,129 @@ func (c *F5XCClient) send(req *http.Request, resData interface{}) error {
 
 	defer res.Body.Close()
 
-	if res.StatusCode < http.StatusOK || res.StatusCode >= http.StatusBadRequest {
-		return fmt.Errorf("unknown error, status code: %d", res.StatusCode)
+	switch res.StatusCode {
+	case http.StatusOK:
+		if err = json.NewDecoder(res.Body).Decode(&resData); err != nil {
+			klog.Error("Error decoding JSON response!")
+
+			return err
+		}
+
+		return nil
+
+	case http.StatusNotFound:
+		resData = nil
+
+		return nil
+
+	case http.StatusUnauthorized:
+		return fmt.Errorf("Credentials not valid!")
+
+	case http.StatusForbidden:
+		return fmt.Errorf("Permission missing for requested resource!")
 	}
 
-	if err = json.NewDecoder(res.Body).Decode(&resData); err != nil {
+	klog.Errorf("Unexpected response!")
+
+	var unexpectedRes interface{}
+	if err = json.NewDecoder(res.Body).Decode(&unexpectedRes); err != nil {
+		klog.Error("Error decoding JSON response!")
+
 		return err
 	}
+	klog.Errorf("%v+", unexpectedRes)
 
-	return nil
+	return fmt.Errorf("Invalid status code: %d", res.StatusCode)
+}
+
+func (c *f5xcClient) getTXTResourceRecord(zone string, rrgroup string, rrname string) (*f5xcTXTResouceRecord, error) {
+	const endpoint = "config/dns/namespaces/system/dns_zones/%s/rrsets/%s/%s/TXT"
+
+	url, err := url.JoinPath(c.BaseURL, fmt.Sprintf(endpoint, zone, rrgroup, rrname))
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &f5xcTXTResouceRecord{}
+
+	err = c.send(req, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (c *f5xcClient) createTXTResourceRecord(zone string, rrgroup string, rrname string, key string) (*f5xcTXTResouceRecord, error) {
+	const endpoint = "config/dns/namespaces/system/dns_zones/%s/rrsets/%s"
+
+	url, err := url.JoinPath(c.BaseURL, fmt.Sprintf(endpoint, zone, rrgroup))
+	if err != nil {
+		return nil, err
+	}
+
+	reqBody := &f5xcTXTResouceRecordCreation{
+		DnsZoneName: zone,
+		GroupName:   rrgroup,
+		RRSet: f5xcTXTrrset{
+			TXTRecord: f5xcTXTRecord{
+				Name:   rrname,
+				Values: []string{key},
+			},
+		},
+	}
+
+	reqBodyBytes, err := json.Marshal(reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(reqBodyBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	res := &f5xcTXTResouceRecord{}
+
+	err = c.send(req, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (c *f5xcClient) updateTXTResourceRecord(zone string, rrgroup string, rrname string, reqBody *f5xcTXTResouceRecord, key string) (*f5xcTXTResouceRecord, error) {
+	const endpoint = "config/dns/namespaces/system/dns_zones/%s/rrsets/%s/%s/TXT"
+
+	url, err := url.JoinPath(c.BaseURL, fmt.Sprintf(endpoint, zone, rrgroup, rrname))
+	if err != nil {
+		return nil, err
+	}
+
+	reqBody.RRSet.TXTRecord.Values = append(reqBody.RRSet.TXTRecord.Values, key)
+
+	reqBodyBytes, err := json.Marshal(reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(reqBodyBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	res := &f5xcTXTResouceRecord{}
+
+	err = c.send(req, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
--- f5xc/types.go
+++ f5xc/types.go
@@ -11,7 +11,7 @@ type F5XCDNSProviderSolver struct {
 	k8sClient *kubernetes.Clientset
 }
 
-type F5XCDNSProviderConfig struct {
+type f5xcDNSProviderConfig struct {
 	// Change the two fields below according to the format of the configuration
 	// to be decoded.
 	// These fields will be set by users in the
@@ -21,10 +21,37 @@ type F5XCDNSProviderConfig struct {
 	TenantName      string                   `json:"tenantName"`
 	ZoneName        string                   `json:"zoneName"`
 	RRGroupName     string                   `json:"rrGroupName"`
+	RRName          string                   `json:"rrName"`
 }
 
-type F5XCClient struct {
+type f5xcClient struct {
 	BaseURL string
 	ApiKey  string
 	Client  *http.Client
+}
+
+type f5xcTXTResouceRecord struct {
+	DnsZoneName string       `json:"dns_zone_name"`
+	GroupName   string       `json:"group_name"`
+	Namespace   string       `json:"namespace"`
+	RecordName  string       `json:"record_name"`
+	Type        string       `json:"type"`
+	RRSet       f5xcTXTrrset `json:"rrset"`
+}
+
+type f5xcTXTrrset struct {
+	Description string        `json:"description"`
+	TTL         int           `json:"ttl"`
+	TXTRecord   f5xcTXTRecord `json:"txt_record"`
+}
+
+type f5xcTXTRecord struct {
+	Name   string   `json:"name"`
+	Values []string `json:"values"`
+}
+
+type f5xcTXTResouceRecordCreation struct {
+	DnsZoneName string       `json:"dns_zone_name"`
+	GroupName   string       `json:"group_name"`
+	RRSet       f5xcTXTrrset `json:"rrset"`
 }
